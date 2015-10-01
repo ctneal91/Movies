9020bb3ff8a8f0828e2555158366775c6dfa7cf7
Christian Neal <c.neal91@gmail.com>
2015-10-01 15:05:06 -0500

    Run both buttons correctly

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,6 +3,7 @@ function Films () {;
 
     var movies = jsonData.movies
 
+    // for low budget movies
     var low = movies.filter(function(movie){
          return movie.budget <= 80;
     });
@@ -11,12 +12,28 @@ function Films () {;
         console.log(movie.title);
         return movie.title;
     });
+
     console.log(lowTitles);
     // var bestMovies = _.filter(movies,)
     lowTitles.forEach(function(movie){
-      var pTag = "<p>" + movie + "</p>";
-      $("#low-budgets").append(pTag)
+      var pTag = "<li>" + movie + "</li>";
+      $("#low-budgets").append(pTag);
+    });
+
+    var leos = movies.filter(function(movie){;
+        return _.contains(movie.stars, 'Leonardo DiCaprio');
+    });
+
+    var leoTitles = leos.map(function(movie){
+      return movie.title;
     })
+
+    console.log(leoTitles)
+
+    leoTitles.forEach(function(movie){
+      var pTag = "<li>" + movie + "</li>";
+      $("#Leos").append(pTag);
+    });
 
   });
 
