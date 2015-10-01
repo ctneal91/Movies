17a9d40f96b0885399ff8a22d293544488103a0b
Christian Neal <c.neal91@gmail.com>
2015-09-30 21:39:24 -0500

    fixed jQuery kind of...

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,14 @@
 function Films () {;
-    $.getJSON("/movies.json").then(function(jsonData){  //  <------
-       jsonData.titles
-                 //  <——— jsonData top-level is an object with an array
-                 //on the ‘movies’ key
-       var low = _.filter(titles, function(movie){;
+  $.getJSON("/movies.json").then(function(jsonData){
+
+    var movies = jsonData.movies
+
+    var low = _.filter(movies, function(movie){
          return movie.budget <= 80;
-       });
+    });
     // var bestMovies = _.filter(movies,)
-      $("#low-budgets").html(42);
+    $("#low-budgets").html(low);
+
     });
 
 };
