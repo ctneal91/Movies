766992c135843b4f7fc0b226d3712c564bddc737
Christian Neal <c.neal91@gmail.com>
2015-09-30 21:33:41 -0500

    Changed jquery line to be able access 'movies' although 'movies is not recognized as a variable

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,14 +1,20 @@
 function Films () {;
-  $.getJSON("/movies.json").then(function(movies){;
-    var low = _.filter(movies, function(movie){;
-      return movie.budget <= 80;
+    $.getJSON("/movies.json").then(function(jsonData){  //  <------
+       jsonData.titles
+                 //  <——— jsonData top-level is an object with an array
+                 //on the ‘movies’ key
+       var low = _.filter(titles, function(movie){;
+         return movie.budget <= 80;
+       });
+    // var bestMovies = _.filter(movies,)
+      $("#low-budgets").html(42);
     });
-    $("#low-budgets").html(low);
-  });
 
 };
 
 
 $(window).ready(function(){
-  $("#button").on("click", Films);
+    $("#button").on("click", function(){
+         Films()
+     });
 });
